b0ea4cbb0a90cd2dc6f2cb28ee372c58d8a8a81c
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-12-30 19:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 //your JS code here. If required.
-const value=document.getElementById("value");
-value.innerText="You are using " + navigator.userAgent + " version " + navigator.appVersion;
+const value=document.getElementById("browser-info");
+// value.innerText="You are using " + navigator.userAgent + " version " + navigator.appVersion;
+
+value.innerHTML=`You are using ${navigator.appName}  version ${navigator.appVersion}`;
